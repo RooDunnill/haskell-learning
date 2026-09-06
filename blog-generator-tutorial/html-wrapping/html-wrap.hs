93cc85d2393068008deacd5353e093957527bd4f
RooDunnill <hyde-academic@proton.me>
2026-09-06 21:07:15 +0100

    created two new types Html and Structure, along with functions to obtain the string stored within them and an appender that appends two Structure types

--- blog-generator-tutorial/html-wrapping/html-wrap.hs
+++ blog-generator-tutorial/html-wrapping/html-wrap.hs
@@ -23,3 +23,24 @@ h1_ = el "h1"
 
 makeHtml :: String -> String -> String
 makeHtml title body = html_ (head_ (title_ title) <> body_ body)  
+
+-- defines new types Html and Structure
+newtype Html = Html String
+newtype Structure = Structure String
+
+-- returns the string embedded in the Structure type
+returnStructureString :: Structure -> String
+returnStructureString struct = 
+    case struct of
+        Structure str -> str
+
+-- returns the string embedded in the Html type
+render :: Html -> String
+render html =
+    case html of
+        Html str -> str
+
+-- appends two structures together
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) = Structure (a <> b)
+
