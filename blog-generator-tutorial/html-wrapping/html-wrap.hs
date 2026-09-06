80a40f2cb22cf5631b5184329466a41a9e222977
RooDunnill <hyde-academic@proton.me>
2026-09-06 20:42:13 +0100

    finished chapter 3.3

--- blog-generator-tutorial/html-wrapping/html-wrap.hs
+++ blog-generator-tutorial/html-wrapping/html-wrap.hs
@@ -3,19 +3,23 @@ main :: IO ()
 main = putStrLn myhtml
 
 myhtml :: String
-myhtml = makeHtml "My Title" "My Body"
+myhtml = makeHtml "My Title" (h1_ "Hello World!" <> p_ "My Body")
 
 el :: String -> String -> String
 el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
 body_ :: String -> String
-body_ content = "<body>" <> content <> "</body>"
+body_ = el "body"
 html_ :: String -> String
-html_ content = "<html>" <> content <> "</html>"
+html_ = el "html"
 title_ :: String -> String
-title_ content = "<title>" <> content <> "</title>"
+title_ = el "title"
 head_ :: String -> String
-head_ content = "<head>" <> content <> "</head>"
+head_ = el "head"
+p_ :: String -> String
+p_ = el "p"
+h1_ :: String -> String
+h1_ = el "h1"  
 
 makeHtml :: String -> String -> String
 makeHtml title body = html_ (head_ (title_ title) <> body_ body)  
