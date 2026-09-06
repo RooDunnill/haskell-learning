bbcb7c1334f5b80647f128ab3aa8644232a9a841
RooDunnill <hyde-academic@proton.me>
2026-09-06 21:17:46 +0100

    added the type TItle that is just a string type and added comments, finished 3.4

--- blog-generator-tutorial/html-wrapping/html-wrap.hs
+++ blog-generator-tutorial/html-wrapping/html-wrap.hs
@@ -1,17 +1,22 @@
 -- should always give the typing of each function beforehand
 main :: IO ()
-main = putStrLn myhtml
+main = putStrLn (render myhtml)
 
-myhtml :: String
+-- calls the function to make the HTML by inputting the Title and the body of text
+myhtml :: Html
 myhtml = makeHtml "My Title" (h1_ "Hello World!" <> p_ "My Body")
 
+-- takes in the title and body as strings and creates the necessary HTML around it before then returning it as an HTML type
+makeHtml :: String -> String -> Html
+makeHtml title body = Html (el "html" (head_ (title_ title) <> body_ body))
+
+-- adds the HTML arrows around the division element you wish to create
 el :: String -> String -> String
 el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
 
+-- a set of functions that creates different HTML structures
 body_ :: String -> String
 body_ = el "body"
-html_ :: String -> String
-html_ = el "html"
 title_ :: String -> String
 title_ = el "title"
 head_ :: String -> String
@@ -21,12 +26,13 @@ p_ = el "p"
 h1_ :: String -> String
 h1_ = el "h1"  
 
-makeHtml :: String -> String -> String
-makeHtml title body = html_ (head_ (title_ title) <> body_ body)  
 
 -- defines new types Html and Structure
 newtype Html = Html String
 newtype Structure = Structure String
+
+-- relables exisitng types to other types that are syntactial the same
+type Title = String
 
 -- returns the string embedded in the Structure type
 returnStructureString :: Structure -> String
